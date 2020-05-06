351584fd6f8887c475c530e68843cd6c7e74f016
LeChase <gps56>
2020-05-05 22:44:02 -0700

    Created Lyrics class and added methods.

--- get_lyrics.py
+++ get_lyrics.py
@@ -2,6 +2,8 @@
 script to obtain lyrics using genius API
 """
 
+import os
+
 import pandas as pd
 import time
 
@@ -21,8 +23,11 @@ def get_lyrics(row):
     try:
         song = genius.search_song(' - '.join((row.artist, row.song)))
         return song.lyrics
-    except AttributeError:
-        return 'NOT FOUND'
+    except (AttributeError, TypeError):
+        return 'NOT FOUND - AttributeError'
+    except TypeError:
+        return 'NOT FOUND - TypeError'
+
 
 
 def main():
@@ -38,6 +43,63 @@ def main():
     uniques.to_pickle(__output__)
     
     print('complete')
+
+
+class Lyrics:
+    # class to gather lyrics on unique artist/song combos from scrobble history
+    # incldues methods to update a previous lyric output with newer scrobble history
+
+    def __init__(self, scrobbles, output = None):
+
+        self.scrobbles = scrobbles
+        self.output = output
+
+        self.scrobbles_df = pd.read_csv(self.scrobbles, header = None, names = ['artist', 'album', 'song', 'datetime'])
+        self.uniques = self.scrobbles_df.drop_duplicates(['artist', 'song'])[['artist', 'song']]
+        if self._has_existing_output():
+            self.output_df = pd.read_pickle(self.output)
+        else:
+            self.output_df = None
+
+    def init_lyrics(self):
+        # initial gather lyrics of unique artist/song combinations
+        df = pd.read_csv(self.scrobbles, header = None, names = ['artist', 'album', 'song', 'datetime'])
+        uniques = df.drop_duplicates(['artist', 'song'])[['artist', 'song']]
+        
+        print(len(uniques), 'unique artist & song combinations')
+        print('starting:', time.ctime())
+        uniques['lyrics'] = uniques.apply(get_lyrics, axis = 1)
+        print('ending:', time.ctime())
+        
+        uniques.to_pickle(__output__)
+        
+        print('complete')
+
+    @staticmethod
+    def get_lyrics(row):
+        try:
+            song = genius.search_song(' - '.join((row.artist, row.song)))
+            return song.lyrics
+        except (AttributeError, TypeError):
+            return 'NOT FOUND - AttributeError'
+        except TypeError:
+            return 'NOT FOUND - TypeError'
+
+
+    def _check_existing_output(self):
+        # read existing output and compare artists/songs to those in the latest scrobbles
+        if self._has_existing_output():
+            uniques = df.drop_duplicates(['artist', 'song'])[['artist', 'song']]
+            
+
+
+    def _has_existing_output(self):
+        if self.output == None:
+            return False
+        else:
+            return os.path.isfile(self.output)
+
+    
 
 
 
