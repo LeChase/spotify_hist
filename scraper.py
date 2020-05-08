703479ace94a962f665e5a106ce8b107dcda1cf5
LeChase <gps56>
2020-05-07 17:49:59 -0700

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,18 @@
+"""
+module containing class for initial scraping of data for artist/songs
+"""
+
+
+class Scraper:
+
+    def __init__(self):
+
+        pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
