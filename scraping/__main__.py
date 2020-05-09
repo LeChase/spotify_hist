0af3c31ba0e0cc4da75f4204140492a1dfe6f6fb
LeChase <gps56>
2020-05-08 21:14:19 -0700

    created __main__ for scraping to be run as a package

---
+++ scraping/__main__.py
@@ -0,0 +1,26 @@
+"""
+call this script to run scraping
+"""
+
+from scraping.scraper import Scraper
+
+from scraping.lyrics import Lyrics
+from scraping.spotify import Spotify
+
+
+
+def main():
+
+    # get lyrics
+    lyric_scraper = Scraper('lyrics', Lyrics, Lyrics.find_lyrics)
+    lyric_scraper.main()
+
+    # get spotify features 
+    spotify_scraper = Scraper('spotify', Spotify, Spotify.get_features)
+    spotify_scraper.main()
+
+
+
+if __name__ == '__main__':
+
+    main()
