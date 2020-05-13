b9c5d753e8f80a48ac0b6bb800e394737c68e62d
LeChase <gps56>
2020-05-12 23:29:15 -0700

    updated script to run collecting lyrics and spotify features

--- main.py
+++ main.py
@@ -3,22 +3,11 @@ main script
 
 """
 
-from scraping.scraper import Scraper
-
-from scraping.lyrics import Lyrics
-from scraping.spotify import Spotify
-
 
 
 def main():
 
-    # get lyrics
-    lyric_scraper = Scraper('lyrics', Lyrics, Lyrics.find_lyrics)
-    lyric_scraper.main()
-
-    # get spotify features 
-    spotify_scraper = Scraper('spotify', Spotify, Spotify.get_features)
-    spotify_scraper.main()
+    pass
 
 
 
