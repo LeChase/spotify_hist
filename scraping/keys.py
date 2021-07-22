7a164c4c785a3cfbbe114f4d635b198af4d3a69c
gpschnaars <46255717+gpschnaars@users.noreply.github.com>
2021-07-22 17:21:38 -0400

    Delete keys.py

--- scraping/keys.py
+++
@@ -1,9 +0,0 @@
-"""
-module containing API keys that i dont want to share
-"""
-
-_genius_key = "UgvKlj9wHavgvBZScdTzfIKL2DKiIatIwPVV0M6JpEqm8YgIC_eJiQsYcsnmU2G-"
-
-_SPOTIPY_CLIENT_ID = '68be2578e21e439a92f5fcc00f9cfb6a'
-_SPOTIPY_CLIENT_SECRET = '45c1b5c64c8e4cbebc3d37c963e04bf9'
-
